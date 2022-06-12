5fb0da7ae0c6ecb4e41abd3d3a5b48b401434ab0
ZF Chung (Zack) <zfchung1@gmail.com>
2022-06-12 17:42:05 +0800

    added status code

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/about', (req, res) => {
+    res.status(200).send('About Page');
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send(`<h1>resource not found</h1>`);
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use for middleware
+// app.listen
