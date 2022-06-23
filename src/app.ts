4c969f4fe3a606d217e8f5cd1a9418f8b2236c69
ZF Chung (Zack) <zfchung1@gmail.com>
2022-06-23 19:25:02 +0800

    Lucas' code review

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import router from "./routes";
 
+// TODO: create this app in a separate file
+// TODO: this app.ts is only concern about how to run it
+// More testable when we do supertest
 const app = express();
+
+// TODO: ideally we maintain port in a config/env variable and only use hard code when no port found (fallback)
 const port = 3000;
 
+// TODO: These middlewares can be considered if they are required in the app creation or during business logic
 // parse form data
 app.use(express.urlencoded({extended: false}));
 // parse json
 app.use(express.json());
 
+
 app.use(router);
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}......`);
--- src/controllers/people/index.ts
+++ src/controllers/people/index.ts
@@ -10,9 +10,18 @@ export function getPeople(req: Request, res: Response) {
 
 export function postPeople(req: Request, res: Response) {
     const {name} = req.body;
+    // TODO: More robust data validation
+    // TODO: There are different data validation; Consider whether the validation can be done earlier before it reaches here.
+    // Is there an email? - Middleware
+    // Is the email in a valid format? - Middleware
+    // Controllers generally does not do any data validation at all....?
+    // Does the email already exist in database? - Business
     if (!name) {
         return res
             .status(400)
+            // TODO: The status code that you chose should be the best representation whether your call is success or not. You should not duplicate and cause inconsistency in the json body
+            // TODO: Ideally you don't just send a human readable code to front end (your machine). But this human readable reason is not consumable by the machine. We should be enumerating the a language/code that the front end can always understand the error. Imagine when our front end application is changing from english to mandarin.
+            // TODO: Consider an entire language service just to translate error code coming from backend to display appropriate information to the user.
             .json({success: false, msg: 'Please provide name value'});
     }
     res.status(201).json({success: true, person: name});
